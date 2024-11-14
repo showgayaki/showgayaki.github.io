c39147b271159c1e8b2c0fa7a21716a1bad5b48d
showgayaki <knk.lee.kj@gmail.com>
2024-11-14 15:58:46 +0900

    隠れQRコードを設置

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,8 +1,24 @@
 // DOMが生成されてからでないとあきまへんで
 window.addEventListener('DOMContentLoaded', () =>{
-    const hamburger = document.getElementById('hamburger');
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('expanded');
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const hamburgerImage = document.getElementById('hamburgerImage');
+    const qrCode = document.getElementById('qrCode');
+
+    hamburgerMenu.addEventListener('click', () => {
+        hamburgerMenu.classList.toggle('expanded');
         document.getElementById('navigation').classList.toggle('show');
+
+        hamburgerImage.classList.add('show');
+        qrCode.classList.remove('show');
+    }, false);
+
+    hamburgerImage.addEventListener('click', () => {
+        hamburgerImage.classList.toggle('show');
+        qrCode.classList.toggle('show');
+    }, false);
+
+    qrCode.addEventListener('click', () => {
+        hamburgerImage.classList.toggle('show');
+        qrCode.classList.toggle('show');
     }, false);
 });
