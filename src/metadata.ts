54e3d2731969cb16c853d2d36100a9057f6793b5
showgayaki <knk.lee.kj@gmail.com>
2026-02-28 23:55:29 +0900

    利用規約作成

--- src/metadata.ts
+++ src/metadata.ts
@@ -16,5 +16,13 @@ export const siteMetadata: {[pathName: string]: PageMetadata} = {
     '/tokushoho': {
         title: '特定商取引法に基づく表記',
         description: 'わたしがつくったアプリに関する特定商取引法に基づく表記です',
+    },
+    '/terms': {
+        title: '利用規約',
+        description: 'わたしがつくったアプリに関する利用規約まとめです',
+    },
+    '/terms/70-calendar': {
+        title: '「70! カレンダー」利用規約',
+        description: '「70! カレンダー」の利用規約です',
     }
 }
