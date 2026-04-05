b3186c50c5f7ee452db76c6308cbf119a04209f5
showgayaki <knk.lee.kj@gmail.com>
2026-04-05 19:46:38 +0900

    ハンバーガーメニューをPC対応しました

--- src/scripts/hamburger.ts
+++ src/scripts/hamburger.ts
@@ -1,24 +1,10 @@
 // DOMが生成されてからでないとあきまへんで
 window.addEventListener('DOMContentLoaded', () =>{
     const hamburgerMenu = document.getElementById('hamburgerMenu');
-    const hamburgerImage = document.getElementById('hamburgerImage');
-    const qrCode = document.getElementById('qrCode');
 
     hamburgerMenu?.addEventListener('click', () => {
         hamburgerMenu.classList.toggle('expanded');
         document.getElementById('navigation')?.classList.toggle('show');
 
-        hamburgerImage?.classList.add('show');
-        qrCode?.classList.remove('show');
-    }, false);
-
-    hamburgerImage?.addEventListener('click', () => {
-        hamburgerImage.classList.toggle('show');
-        qrCode?.classList.toggle('show');
-    }, false);
-
-    qrCode?.addEventListener('click', () => {
-        hamburgerImage?.classList.toggle('show');
-        qrCode.classList.toggle('show');
     }, false);
 });
